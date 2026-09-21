5bec6dd90b2802fbfa8d863fac67de3c44ea3b77
Rayan "IcyAuk" N.V. <92365145+IcyAuk@users.noreply.github.com>
2026-09-21 18:43:59 +0200

    Update postal codes list in script.js

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const nafs = ["62.01Z","58.29C","73.11Z"];
-const cps  = [/*"74200",*/"74140"/*,"74500","74100","74160"*/];
+const cps = [
+  // Léman : Annemasse – Thonon – Évian
+  "74100", // Annemasse, Ambilly, Ville-la-Grand, Vétraz-Monthoux
+  "74200", // Thonon-les-Bains
+  "74500", // Évian, Publier, Amphion
+  // Vallée de l'Arve
+  "74930", // Reignier-Ésery
+  "74800", // La Roche-sur-Foron
+  "74130", // Bonneville
+  "74970", // Marignier
+  "74300", // Cluses
+  "74700", // Sallanches
+  "74170", // Saint-Gervais-les-Bains / Le Fayet
+  // Annecy
+  "74000", // Annecy centre
+  "74940", // Annecy-le-Vieux
+  "74960", // Cran-Gevrier, Meythet
+  "74600", // Seynod
+  "74370", // Pringy
+];
 const rows = [["Nom","Adresse","NAF","Effectif"]];
 for (const naf of nafs) for (const cp of cps) for (let p = 1; p <= 5; p++) {
   const r = await (await fetch(`/search?activite_principale=${naf}&code_postal=${cp}&per_page=25&page=${p}`)).json();
